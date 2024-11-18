0615df13c502a02d0d2496ebca2f4a839f5866be
RenanAndrade <renan.amedeiros@outlook.com>
2024-11-18 18:40:52 -0300

    feat(deploy): realiza o deploy para produção

--- trip-explore/src/backend/server.js
+++ trip-explore/src/backend/server.js
@@ -4,12 +4,12 @@ const path = require('path');
 require('dotenv').config(); // Carregar variáveis de ambiente do .env
 const authRoutes = require('./rotas/authRoutes'); // Rotas de autenticação
 const geminiRoutes = require('./rotas/GeminiRoutes'); // Rotas do Gemini
-const logsRoutes = require('./rotas/LogsRoutes'); // Certifique-se de importar corretamente
+const logsRoutes = require('./rotas/LogsRoutes'); // Rota do log corretamente
 
 const app = express();
 
 // Middleware
-app.use(cors()); // Permitir requisições de outras origens (necessário em dev)
+app.use(cors()); // Permitir requisições de outras origens 
 app.use(express.json()); // Permitir leitura de JSON no corpo das requisições
 
 // Verificar se JWT_SECRET está carregado
@@ -23,16 +23,17 @@ app.use('/api/auth', authRoutes);
 app.use('/api/travel', geminiRoutes); // Adiciona as rotas do Gemini
 app.use('/api', logsRoutes); // Rota para logs
 
-// Em produção, servir arquivos estáticos do React
+// Servir arquivos estáticos do React para produção
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../frontend/build')));
+  app.use(express.static(path.join(__dirname, 'build'))); // Configurado para a pasta build no mesmo nível do server.js
 
   // Rota "catch-all" para o React
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
   });
 }
 
+// Configuração da porta
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
